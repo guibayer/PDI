658597a19fec0aa118723a965d7e938d28ee7b30
guibayer <guibayer2@gmail.com>
2019-06-07 09:22:06 -0300

    Update aula0706_operacoes_morfologicas_2.m

--- aula0706_operacoes_morfologicas_2.m
+++ aula0706_operacoes_morfologicas_2.m
@@ -10,8 +10,8 @@ iopen = imopen(i,se);
 % Exercício 2
 
 m = imread('mamo.jpeg');
-m = rgb2gray(m);
-se1 = strel('line',10);
+%m = rgb2gray(m); Já está em tons de cinza
+se1 = strel('line',10, 0); %Segundo argumento é o angulo(que no exerc ele pede para testar com 0,45,90,180)
 m_se = imopen(m, se);
 m_se1 = imopen(m, se1);
 subplot(1,2,1),imshow(m_se);
